c0377238499e7a17ef03ab814c80952383c3b0cb
Nuralili <sajmerdennurli@gmail.com>
2025-10-15 12:52:38 +0500

    Update web.py

--- web.py
+++ web.py
@@ -8,3 +8,13 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+import asyncio
+from bot import start_bot  # импортируем функцию запуска из bot.py
+
+if __name__ == "__main__":
+    # Запускаем бота и сайт вместе
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())  # бот работает в фоне
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+
